e9bdc94ba491b8939ad57417050edbe053bdff12
Diego Freniche <diego@freniche.com>
2023-03-15 10:27:19 +0100

    Added update sample code

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-SOURCE_FILES="insert find bcon list_collections hello_mongoc delete"
+# Bash script to build and run all sample code
 
+if [ ! -f "URI.h" ]; then
+cat << EOF
+Error: You need to create a URI.h file containing the Connection String URI of your MongoDB instance.
+
+Sample URI.h file:
+
+// Start URI.h
+#define MY_MONGODB_URI "mongodb+srv://YOUR-MONGODB-CONNECTION-STRING-URI"
+// End URI.h
+
+To get your connection string please read: https://www.mongodb.com/basics/mongodb-connection-string
+EOF
+exit
+fi
+
+# list of files to be compiled and run
+SOURCE_FILES="insert find bcon list_collections hello_mongoc delete update"
+
+if [ ! -d "output" ]; then  
 mkdir output
+else
+rm output/*
+fi
 
 echo COMPILING ALL EXAMPLES!
 
